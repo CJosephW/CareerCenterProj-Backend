aef1a806f27eecc748ef8533574f357327785b6f
Cameron Wise <wisecameron1@gmail.com>
2020-01-08 15:09:42 +0000

    GET /skills route working

--- populate.js
+++ populate.js
@@ -1,14 +1,6 @@
 import mongoose from 'mongoose';
 import Student from './models/student';
 
-const students = [
-	{
-		name: 'Michael Ball',
-		skills: [
-			['Michael Ball', '2342234', '10/2/2019','good','well', 'Jerry Simons', '10/20/2019']
-		]
-	},
-];
 
 mongoose.connect('mongodb://localhost/students');
 
--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 import express, { Router } from 'express';
-
+import Skill from './models/skillsModel'
 import { index } from './controllers/students';
 
 const router = Router();
@@ -10,7 +10,30 @@ router.route('/skills')
     .post((req, res) => {
         console.log(req.body);
 
-        res.json(req.body)
+        res.json(req.body);
+
+        var myData = new Skill();
+        myData.title = req.body.title;
+        myData.evaluator_instructions = req.body.evaluator_instructions;
+        myData.cadidate_directives = req.body.cadidate_directives;
+        myData.tasks = req.body.tasks;
+        myData.save()
+        .then(item =>{
+            res.send("Item saved to database");
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database")
+        })
+
+    })
+    .get(async(req, res)=>{
+        try{
+            const found_skills = await Skill.find()
+            res.json(found_skills)
+        } catch(err){
+            res.status(500).json({message: err.message})
+        }
     });
+
 
 export default router;
